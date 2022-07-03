9d83a6fa26f39934fc29dece1e4d4ef08923a584
Rohit Gour <rohitgourrg999@gmail.com>
2022-07-03 18:20:43 +0530

    fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import './App.css';
+import React from 'react';
+
+import RandomBtn from './components/RandomBtn';
+import RandomQuotePage from './pages/RandomQuotePage';
+import AuthorQuotesPage from './pages/AuthorQuotesPage';
 
 function App() {
+
+  const [randomQuoteData, setRandomQuoteData] = React.useState({})
+  const [isRandomQuote, setIsRandomQuote] = React.useState(true)
+
+  React.useEffect(() => {
+    getRandomQuote()
+  }, [])
+
+  const getRandomQuote = async () => {
+    setIsRandomQuote(true)
+    const resp = await fetch('https://quote-garden.herokuapp.com/api/v3/quotes/random')
+    const data = await resp.json()
+    setRandomQuoteData(() => data.data[0])
+  }
+
   return (
-    <div className="App">
-      <h1 className='bg-orange-200'>Har Har Mahadev 🙏</h1>
+    <div className="font-raleway grid place-items-center relative min-h-screen">
+      <RandomBtn getRandomQuote={getRandomQuote} />
+      <main className='mt-32 mb-10 md:mt-48 md:mb-20'>
+        {
+          isRandomQuote ?
+            <RandomQuotePage 
+              randomQuoteData={randomQuoteData}
+              setIsRandomQuote={setIsRandomQuote} /> :
+            <AuthorQuotesPage 
+              author={randomQuoteData.quoteAuthor} 
+            />
+        }
+      </main>
+
+      <footer className='flex justify-center items-center gap-2 w-full py-4 text-center text-[#828282]'>
+        <span>Created by </span>
+        <a href='https://devchallenges.io/portfolio/Rohitgour03' className='font-semibold'>Rohitgour03</a>
+        <a href="https://devchallenges.io">devChallenges.io</a> 
+      </footer>
     </div>
   );
 }
